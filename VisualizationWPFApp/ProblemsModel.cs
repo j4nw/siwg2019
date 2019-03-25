6c065222f2b46c948820b8a1a09a23d96a1a01f9
Mateusz Ciesielski <mateusz.ciesielski@interia.pl>
2019-03-25 22:31:08 +0100

    Add problems combobox

---
+++ VisualizationWPFApp/ProblemsModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualizationWPFApp
+{
+    public class ProblemsModel
+    {
+        public List<string> Problems { get; private set; }
+        public string SelectedProblem { get; set; }
+
+        public ProblemsModel()
+        {
+            Problems = new List<string>();
+            Problems.Add("Plansza 2D");
+            Problems.Add("Warcaby");
+            Problems.Add("Kółko i krzyżyk");
+            Problems.Add("Labirynt");
+            SelectedProblem = Problems[0];
+        }
+    }
+}
---
+++ VisualizationWPFApp/ProblemsViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualizationWPFApp
+{
+    public class ProblemsViewModel : INotifyPropertyChanged
+    {
+        private ProblemsModel model = new ProblemsModel();
+
+        public List<string> Problems { get { return model.Problems; } }
+
+        public string SelectedProblem
+        {
+            get
+            {
+                return model.SelectedProblem;
+            }
+            set
+            {
+                model.SelectedProblem = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("SelectedProblem"));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
