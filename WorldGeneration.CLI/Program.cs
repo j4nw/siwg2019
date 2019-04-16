c86627a9e51a579763484dadc9d888f12051a30c
Jan Wojtecki <janwojtecki@gmail.com>
2019-04-16 12:41:51 +0200

    wall visualization example

--- WorldGeneration.CLI/Program.cs
+++ WorldGeneration.CLI/Program.cs
@@ -11,15 +11,18 @@ namespace WorldGeneration.CLI
         {
             var world = new World(1024, 512);
             world.GeneratePerlinTerrain();
+            world.DrawRectangle(100, 100, 100, 100, WorldLayer.Wall);
 
             var colorsByLayer = new Dictionary<WorldLayer, (Color low, Color high)>
             {
+                {WorldLayer.Wall, (Color.Black, Color.Black)},
                 {WorldLayer.Water, (Color.DodgerBlue, Color.DodgerBlue)},
                 {WorldLayer.Land, (Color.DarkOliveGreen, Color.GreenYellow)}
             };
 
             var orderedLayers = new List<WorldLayer>
             {
+                WorldLayer.Wall,
                 WorldLayer.Water,
                 WorldLayer.Land
             };
